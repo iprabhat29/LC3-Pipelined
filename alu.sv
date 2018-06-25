0e081d5b291929a0b24539f6163ff5711c4edc1c
iprabhat29 <36540010+iprabhat29@users.noreply.github.com>
2018-06-25 12:11:15 -0400

    Delete alu.sv

--- alu.sv
+++
@@ -1,6 +0,0 @@
-module execution(lc3Interface.ALU inf);
-  always @(posedge inf.clock) begin
-    @(inf.Decode_done == 1);
-    $display("DR %d   SR1 %d    SR2 %d   Imm %d",inf.DR,inf.SR1,inf.SR2,inf.imm);
-  end
-endmodule
