fbcc82f622202ad7fd607dd8da364f68cfc56440
iprabhat29 <36540010+iprabhat29@users.noreply.github.com>
2018-06-25 12:13:00 -0400

    Delete fetch.sv

--- fetch.sv
+++
@@ -1,8 +0,0 @@
-module fetch(lc3Interface.FETCH inf);
-  always @(posedge inf.clock) begin
-    wait (inf.Decode_done == 1);
-    @(posedge inf.clock);
-    inf.PC = inf.PC + 4;
-    end
-endmodule
-    
